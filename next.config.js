e22300193b31dcd71052762dc8cd5e871c59573c
Abdurehman Saleemi <76134547+AbdurehmanSaleemi@users.noreply.github.com>
2023-12-22 21:34:36 +0500

    Add Next.js configuration options

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    reactStrictMode: true,
+    runtime: 'edge',
+    unstable_allowDynamic: [
+        // use a glob to allow anything in the function-bind 3rd party module
+        '/node_modules/function-bind/**',
+    ],
+}
 
 module.exports = nextConfig
