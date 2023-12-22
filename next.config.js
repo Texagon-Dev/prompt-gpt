f50e614e6d349b73c8f4bdf63ed84299f69f0ec2
AbdurehmanSaleemi <asaleemi121@gmail.com>
2023-12-22 23:14:12 +0500

    Remove unused configuration options

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-    runtime: 'edge',
-    unstable_allowDynamic: [
-        // use a glob to allow anything in the function-bind 3rd party module
-        '/node_modules/function-bind/**',
-    ],
 }
 
 module.exports = nextConfig
